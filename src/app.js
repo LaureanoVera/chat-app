316014921ba3cedeaf0fbfaaa07ac339717809d8
LaureanoVera <laureanoigvera@gmail.com>
2021-07-13 08:40:41 -0300

    instalacion y configuracion de vue

--- src/app.js
+++ src/app.js
@@ -6,13 +6,16 @@ const port = 3000;
 // VIEWS ENGINE
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "..", "/public/views"));
+
 // STATIC
 app.use(express.static(path.join(__dirname, "..", "/public")));
 
+// ROUTE
 app.get("/", (req, res) => {
   res.render("index");
 });
 
+// SERVER LISTENING
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
